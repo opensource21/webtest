9626b3b5700a3469dcb6c2145b2b9a636813d292
niels <opensource21@gmail.com>
2015-04-21 11:38:18 +0200

    Rule for Webdriver-Access.

---
+++ src/main/java/de/ppi/selenium/junit/WebDriverRule.java
@@ -0,0 +1,27 @@
+package de.ppi.selenium.junit;
+
+import org.junit.rules.ExternalResource;
+
+import de.ppi.selenium.browser.SessionManager;
+import de.ppi.selenium.browser.WebBrowser;
+
+public class WebDriverRule extends ExternalResource {
+
+	private static volatile long nrOfTests = 0;
+	//TODO: This should be configurable vai Annotations.
+	private static final long MAX_NR_OF_REUSE = 10;
+
+	@Override
+	protected void before() throws Throwable {
+		nrOfTests++;
+		if (nrOfTests > MAX_NR_OF_REUSE) {
+			final SessionManager manager = SessionManager.getInstance();
+			final WebBrowser browser = manager.getCurrentSession(false);
+			if (browser != null) {
+				manager.removeSession(browser);
+				browser.quit();
+			}
+		}
+	}
+
+}
