9727ed2031a63ef9440ad5c36e911f8622c9dcef
niels <opensource21@gmail.com>
2015-08-24 18:02:57 +0200

    Move Fragment to webtest.

---
+++ src/main/java/org/selophane/elements/base/Fragment.java
@@ -0,0 +1,28 @@
+package org.selophane.elements.base;
+
+import org.openqa.selenium.support.PageFactory;
+import org.selophane.elements.factory.api.ChainedElementLocatorFactory;
+import org.selophane.elements.factory.api.ElementDecorator;
+
+/**
+ * Baseclass of a fragment which handles the initialization.
+ *
+ */
+public class Fragment extends ElementImpl {
+
+    /**
+     *
+     * Initiates an fragment.
+     *
+     * @param elementLocator the locator of the webelement.
+     */
+    public Fragment(final UniqueElementLocator elementLocator) {
+        super(elementLocator);
+        PageFactory
+                .initElements(
+                        new ElementDecorator(
+                                elementLocator.getWebDriver(),
+                                new ChainedElementLocatorFactory(elementLocator)),
+                        this);
+    }
+}
