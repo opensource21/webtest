bdedbdaf80acf9c2376d0913f590a635a6999f69
niels <opensource21@gmail.com>
2015-08-24 14:51:57 +0200

    JavaDoc

--- src/main/java/de/ppi/selenium/logevent/api/EventLoggerFactory.java
+++ src/main/java/de/ppi/selenium/logevent/api/EventLoggerFactory.java
@@ -3,63 +3,103 @@ package de.ppi.selenium.logevent.api;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * Class EventLoggerFactory
+ * Factory to create {@link EventLogger}.
  *
  */
-// TODO JAVADOC .
 public final class EventLoggerFactory {
 
     /**
-     * The Logger for the controller.
+     * Map which defines priorities on which the event should be logged. If the
+     * logevent has a priotity higher or equal then the defined priority, the
+     * event will be logged.
      */
-    private static final Logger LOG = LoggerFactory
-            .getLogger(EventLoggerFactory.class);
-
-    private static final Map<String, Priority> GLOBAL_PRIORITIES =
+    private static final Map<String, Priority> PRIORITIES =
             new HashMap<String, Priority>();
 
-    private static final Map<String, Priority> GLOBAL_SCREENSHOT_PRIORITIES =
+    /**
+     * Map which defines priorities on which a screenshot should be created. If
+     * the logevent has a priotity higher or equal then the defined priority, a
+     * screenshot will maked.
+     */
+    private static final Map<String, Priority> SCREENSHOT_PRIORITIES =
             new HashMap<String, Priority>();
 
-    private static EventStorage STORAGE;
+    /** The storage system. */
+    private static EventStorage storage;
 
+    /** The source of the event. */
     private final EventSource eventSource;
 
     /**
      * Initiates an object of type EventLoggerFactory.
      *
-     * @param eventSource
+     * @param eventSource the source of the event.
      */
     private EventLoggerFactory(EventSource eventSource) {
         super();
         this.eventSource = eventSource;
     }
 
+    /**
+     * Creates an {@link EventLogger} with priority {@link Priority#DEBUG}.
+     *
+     * @param group the group of the event.
+     * @param item the item of the event.
+     * @return a {@link EventLogger}.
+     */
     public EventLogger onDebug(String group, String item) {
         return on(Priority.DEBUG, group, item);
     }
 
+    /**
+     * Creates an {@link EventLogger} with priority
+     * {@link Priority#DOCUMENTATION}.
+     *
+     * @param group the group of the event.
+     * @param item the item of the event.
+     * @return a {@link EventLogger}.
+     */
     public EventLogger onDoku(String group, String item) {
         return on(Priority.DOCUMENTATION, group, item);
     }
 
+    /**
+     * Creates an {@link EventLogger} with priority {@link Priority#FAILURE}.
+     *
+     * @param group the group of the event.
+     * @param item the item of the event.
+     * @return a {@link EventLogger}.
+     */
     public EventLogger onFailure(String group, String item) {
         return on(Priority.FAILURE, group, item);
     }
 
+    /**
+     * Creates an {@link EventLogger} with priority {@link Priority#EXCEPTION}.
+     *
+     * @param group the group of the event.
+     * @param item the item of the event.
+     * @return a {@link EventLogger}.
+     */
     public EventLogger onException(String group, String item) {
         return on(Priority.EXCEPTION, group, item);
     }
 
+    /**
+     * Creates an {@link EventLogger} with given priority.
+     *
+     * @param priority the priority of the {@link EventLogger}.
+     * @param group the group of the event.
+     * @param item the item of the event.
+     * @return a {@link EventLogger}.
+     */
     public EventLogger on(Priority priority, String group, String item) {
-        if (priority.isMoreImportantThan(getPriority(GLOBAL_PRIORITIES,
-                eventSource, group, item))) {
-            return new EventLoggerImpl(STORAGE, priority, getPriority(
-                    GLOBAL_SCREENSHOT_PRIORITIES, eventSource, group, item),
+        if (priority.isMoreImportantThan(
+                getPriority(PRIORITIES, eventSource, group, item))) {
+            return new EventLoggerImpl(
+                    storage, priority, getPriority(SCREENSHOT_PRIORITIES,
+                            eventSource, group, item),
                     eventSource, group, item);
         } else {
             return new EmptyLogger();
@@ -101,8 +141,8 @@ public final class EventLoggerFactory {
      * @param item the item of the group.
      * @return the key.
      */
-    private String
-            createKey(EventSource eventSource, String group, String item) {
+    private String createKey(EventSource eventSource, String group,
+            String item) {
         final StringBuilder keyBuilder = new StringBuilder(eventSource.name());
         if (group != null) {
             keyBuilder.append('.').append(group);
@@ -113,6 +153,12 @@ public final class EventLoggerFactory {
         return keyBuilder.toString();
     }
 
+    /**
+     * Create an instance, so it's more the common logging feeling.
+     *
+     * @param source the event-source.
+     * @return the factory.
+     */
     public static EventLoggerFactory getInstance(EventSource source) {
         return new EventLoggerFactory(source);
     }
@@ -120,8 +166,8 @@ public final class EventLoggerFactory {
     /**
      * @param storage the {@link EventStorage} to set.
      */
-    public static synchronized void setSTORAGE(EventStorage storage) {
-        STORAGE = storage;
+    public static synchronized void setStorage(EventStorage storage) {
+        EventLoggerFactory.storage = storage;
     }
 
     /**
@@ -131,11 +177,11 @@ public final class EventLoggerFactory {
      */
     public void setDefaultPriority(Priority priority) {
         for (EventSource source : EventSource.values()) {
-            GLOBAL_PRIORITIES.put(source.name(), priority);
+            PRIORITIES.put(source.name(), priority);
         }
-        if (GLOBAL_SCREENSHOT_PRIORITIES.isEmpty()) {
+        if (SCREENSHOT_PRIORITIES.isEmpty()) {
             for (EventSource source : EventSource.values()) {
-                GLOBAL_SCREENSHOT_PRIORITIES.put(source.name(), priority);
+                SCREENSHOT_PRIORITIES.put(source.name(), priority);
             }
         }
     }
@@ -152,9 +198,9 @@ public final class EventLoggerFactory {
     public void setPriority(Priority priority, EventSource eventSource,
             String group, String item) {
         final String key = createKey(eventSource, group, item);
-        GLOBAL_PRIORITIES.put(key, priority);
-        if (!GLOBAL_SCREENSHOT_PRIORITIES.containsKey(key)) {
-            GLOBAL_SCREENSHOT_PRIORITIES.put(key, priority);
+        PRIORITIES.put(key, priority);
+        if (!SCREENSHOT_PRIORITIES.containsKey(key)) {
+            SCREENSHOT_PRIORITIES.put(key, priority);
         }
     }
 
@@ -165,7 +211,7 @@ public final class EventLoggerFactory {
      */
     public void setDefaultScreenPriority(Priority priority) {
         for (EventSource source : EventSource.values()) {
-            GLOBAL_SCREENSHOT_PRIORITIES.put(source.name(), priority);
+            SCREENSHOT_PRIORITIES.put(source.name(), priority);
         }
     }
 
@@ -181,7 +227,7 @@ public final class EventLoggerFactory {
     public void setScreenshotPriority(Priority priority,
             EventSource eventSource, String group, String item) {
         final String key = createKey(eventSource, group, item);
-        GLOBAL_SCREENSHOT_PRIORITIES.put(key, priority);
+        SCREENSHOT_PRIORITIES.put(key, priority);
     }
 
 }
