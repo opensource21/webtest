2a862de3dcac168bcc68621eff3431a14d334110
niels <opensource21@gmail.com>
2015-09-08 17:01:06 +0200

    Disable Autoclose on Exit

--- src/test/java/org/selophane/elements/WebTestConstants.java
+++ src/test/java/org/selophane/elements/WebTestConstants.java
@@ -11,5 +11,5 @@ import de.ppi.selenium.logevent.backend.H2EventStorage;
 public interface WebTestConstants {
     /** The on instance of {@link EventLogRule}. */
     EventLogRule EVENT_LOG_RULE = new EventLogRule(new H2EventStorage(
-            "jdbc:h2:mem:test;MODE=PostgreSQL", "", ""));
+            "jdbc:h2:mem:test;MODE=PostgreSQL;DB_CLOSE_ON_EXIT=FALSE", "", ""));
 }
